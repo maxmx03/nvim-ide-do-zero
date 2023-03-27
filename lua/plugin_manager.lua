c9ffb5211baafd15f7822e23744fd158e0ed5275
Max Miliano <milianordelcanto@gmail.com>
2023-03-27 10:42:45 -0300

    update

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -27,6 +27,24 @@ local plugins = {
       'nvim-lua/plenary.nvim'
     },
   },   
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+    }
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'saadparwaiz1/cmp_luasnip'
+    }
+  }
 }
 
 lazy.setup(plugins)
